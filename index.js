d4db078f4451cd7bf0b7d7cf2983322b3b7b3d96
Khandaker Samin Yeasar <138380913+KhandakerSamin@users.noreply.github.com>
2023-11-08 15:59:06 +0600

    Jwt token created and set in application

--- index.js
+++ index.js
@@ -45,7 +45,15 @@ async function run() {
     app.post('/jwt', async(req, res ) => {
         const user = req.body;
         console.log("user for token", user);
-        const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{exprieIn:'1h'})
+        const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
+
+        res.cookie('token' ,token, {
+          httpOnly: true,
+          secure: false
+        })
+
+        .send({success : true })
+
     } )
     
     
